f0ae37f36cd8e72d7bd68488a1de6aec410d3c0a
RyanKay <48168596+RehanTharusha@users.noreply.github.com>
2024-03-29 11:17:16 +0530

    if else statements

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,27 @@
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+// const BMIMark = massMark / (heightMark * heightMark);
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// console.log(BMIMark, BMIJohn);
+
+// /* Write your code below. Good luck! 🙂 */
+
+// if (BMIJohn > BMIMark) {
+//     console.log(`Mark's BMI (${BMIMark}) is higher than John's(${BMIJohn})`)
+// }else{
+//     console.log(`John's BMI (${BMIJohn}) is higher than Mark's(${BMIMark})`)
+// }
+
+
+// const money = 0
+
+// if (money) {
+//     console.log("Don't spend it all!");
+// }else{
+//     console.log("Get a job!");
+// }
+
+
