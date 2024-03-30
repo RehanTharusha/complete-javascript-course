8695b93a5822b2ab42933a61bfb15680e56b3a5e
RyanKay <48168596+RehanTharusha@users.noreply.github.com>
2024-03-31 01:30:52 +0530

    started PROJECT #1

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+// document.querySelector('.guess').value = 23;
+
+// console.log((document.querySelector('.guess').value = 23));
+
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+});
