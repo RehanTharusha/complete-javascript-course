7963f8c73ddc80af2970f9bbc7b9f6eb386dd9e0
RyanKay <48168596+RehanTharusha@users.noreply.github.com>
2024-03-31 22:09:57 +0530

    updates to the game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -8,22 +8,38 @@ const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
+console.log(secretNumber);
 
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = document.querySelector('.guess').value;
+const message = document.querySelector('.message');
+
+function checkGuess() {
+  const guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
-    document.querySelector('.message').textContent = 'Please input a number...';
+    message.textContent = 'Please input a number...';
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct Number!';
+    message.textContent = 'Correct Number!';
+
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Too High!';
+    message.textContent = 'Too High!';
     score--;
     document.querySelector('.score').textContent = score;
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = 'Too Low!';
+    message.textContent = 'Too Low!';
     score--;
     document.querySelector('.score').textContent = score;
+  }
+}
+
+document.querySelector('.check').addEventListener('click', checkGuess);
+
+document.addEventListener('keypress', function (event) {
+  // Check if the pressed key is Enter (keycode 13)
+  if (event.key === 'Enter') {
+    // Call the checkGuess function
+    checkGuess();
   }
 });
