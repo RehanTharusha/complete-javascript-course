fa530ec6fbc8a64740dad620f8b8c62b3be32cf5
RyanKay <48168596+RehanTharusha@users.noreply.github.com>
2024-03-31 15:58:13 +0530

    game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -4,6 +4,26 @@
 
 // console.log((document.querySelector('.guess').value = 23));
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
-  console.log(document.querySelector('.guess').value);
+  const guess = document.querySelector('.guess').value;
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Please input a number...';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too High!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too Low!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
 });
